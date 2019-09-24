e962b8a761c38d25d2a2f6e25785bc91e86f2e9b
Julian Nuñez <julian.nunezm@gmail.com>
2019-09-24 13:37:32 +1000

    Adding Lexi to Installed_Apps, defining its models, and making the migration

--- lexi/models.py
+++ lexi/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Message_Analysis(models.Model):
+    input_text = models.CharField(max_length = 5000)
+    output_text = models.CharField(max_length = 5000)
+    total_words = models.IntegerField(default=0)
+    common_words = models.IntegerField(default=0)
+    uncommon_words = models.IntegerField(default=0)
+    business_words = models.IntegerField(default=0)
+    analysis_date = models.DateTimeField('analysis date')
+
+    def __str__(self):
+        return self.output_text
+    def is_accessible(self):
+        return ((self.common_words + self.business_words)/self.total_words) >= 0.8  #Higher than 80%
+
+class Common_Word(models.Model):
+    word = models.CharField(primary_key=True, max_length = 20)
+    times = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.word
+
+class Business_Word(models.Model):
+    word = models.CharField(primary_key=True, max_length = 20)
+    times = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return self.word
