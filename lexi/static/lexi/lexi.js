19f8a8d220a787f265b0030860ad8cb83f3a1b21
Julian Nunez <julian.nunezm@gmail.com>
2020-01-09 17:21:07 +1100

    Add js functions.

---
+++ lexi/static/lexi/lexi.js
@@ -0,0 +1,6 @@
+console.log("Lexi.js...");
+/*$("#oneK_button").click(function(){
+    console.log("First");
+    //this.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>';
+    console.log(this.innerHTML);
+});*/
