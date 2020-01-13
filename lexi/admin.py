35d360bcec97e121df2a55206aa94d0d4f93ed10
Julian Nunez <julian.nunezm@gmail.com>
2020-01-13 20:28:16 +1100

    Add filters and formats for admin module.

--- lexi/admin.py
+++ lexi/admin.py
@@ -6,4 +6,13 @@ from .models import Business_Word, Message_Analysis, Configuration, Word
 admin.site.register(Business_Word)
 admin.site.register(Message_Analysis)
 admin.site.register(Configuration)
-admin.site.register(Word)
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ['word', 'is_common', 'is_business_word', 'creation_datetime', 'creation_user']
+    ordering = ['word']
+    list_per_page = 50
+    search_fields = ['word']
+    list_filter = ['is_common', 'is_business_word', 'creation_datetime', 'creation_user']
+    fields = ['is_common', 'is_business_word']
+
+admin.site.register(Word, WordAdmin)
