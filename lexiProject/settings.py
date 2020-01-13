7c97df4c976e48792a731ed60d0fedc6f5728364
Julian Nunez <julian.nunezm@gmail.com>
2020-01-13 13:28:42 +1100

    Set Postgresql connection and test it.

--- lexiProject/settings.py
+++ lexiProject/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'lexi.apps.LexiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lexi.apps.LexiConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'lexiProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lexi_db',
+        'USER': 'vicuyzb',
+        'PASSWORD': 'vicuyzb',
+        #'HOST': '127.0.0.1',
+        #'PORT': '5432',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
